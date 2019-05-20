a6c1b0ff69240e2e647324f8132804e713bfe331
Jack Parkinson <jackparkinson792@gmail.com>
2019-05-20 22:13:25 +0100

    Search fixed

--- src/utils/Conversions.java
+++ src/utils/Conversions.java
@@ -1,20 +1,20 @@
 package utils;
 
 public class Conversions {
-    private static double kmtomiles_mult = 0.621371;
-    private static double kelvintocelsius_add = -273.15;
-    private static double kelvintofahrenheit_mult = 1.8;
-    private static double kelvintofahrenheit_add = -459.67;
 
-    public static double toMiles(double kilometres){
-        return kilometres * kmtomiles_mult;
+    public static double toKilometres(double metres){
+        return metres / 1000;
+    }
+
+    public static double toMiles(double metres){
+        return metres / 1000 * 0.621371;
     }
 
     public static double toCelsius(double kelvin){
-        return kelvin + kelvintocelsius_add;
+        return kelvin -273.15;
     }
 
     public static double toFahrenheit(double kelvin){
-        return (kelvin * kelvintofahrenheit_mult) + kelvintofahrenheit_add;
+        return (kelvin * 1.8) - 459.67;
     }
 }
