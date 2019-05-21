8e29120f367cb5bef55abd0b999de869404d4265
Gellért Peresztegi-Nagy <pereszteginagy.gellert@gmail.com>
2019-05-21 10:31:16 +0100

    Fixed settings page

--- src/sample/Settingspage.java
+++ src/sample/Settingspage.java
@@ -1,11 +1,29 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import utils.Settings;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class Settingspage {
+public class Settingspage implements Initializable {
+
+    @FXML
+    private ToggleButton tempCelsius;
+
+    @FXML
+    private ToggleButton tempFahr;
+
+    @FXML
+    private ToggleButton distKilom;
+
+    @FXML
+    private ToggleButton distMiles;
 
     public void back() throws IOException {
         Main.navigateBackToMainPage();
@@ -25,5 +43,20 @@ public class Settingspage {
 
     public void btnMiles() {
         Settings.setDistanceUnitPreference(Settings.DistanceUnit.MILE);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if(Settings.getTemperatureUnitPreference() == Settings.TemperatureUnit.CELSIUS){
+            tempCelsius.setSelected(true);
+        } else {
+            tempFahr.setSelected(true);
+        }
+
+        if(Settings.getDistanceUnitPreference() == Settings.DistanceUnit.METER){
+            distKilom.setSelected(true);
+        } else {
+            distMiles.setSelected(true);
+        }
     }
 }
